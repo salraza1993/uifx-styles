f1b11b41c20e07e8e3653c2f0af2c672f15d42b5
Salman Raza <salraza1993@gmail.com>
2026-02-16 14:27:04 +0400

    npm packages adeed

---
+++ release.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true
+      }
+    ],
+    '@semantic-release/github'
+  ]
+};
